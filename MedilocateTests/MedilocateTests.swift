716c1a91a2d712df649825ee5e6cc366fdd2d590
niqretnuh <niqretnuh@gmail.com>
2025-02-03 18:31:59 -0600

    added interface

---
+++ MedilocateTests/MedilocateTests.swift
@@ -0,0 +1,16 @@
+//
+//  MedilocateTests.swift
+//  MedilocateTests
+//
+//  Created by Haoran 22. Qin on 2/3/25.
+//
+
+import Testing
+
+struct MedilocateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
